c030b231165369d9f1d451321b9bb0566c4aa7db
Peter Vu <vutuananh.vn.sg@gmail.com>
2020-12-22 23:50:22 +0800

    Final commit

---
+++ src/components/Results.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="results">
+      <p>Reaction time: {{score}} ms</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Results',
+    props: ['score'],
+    data() {
+        return {
+            // reactionTime: null
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
