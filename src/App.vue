c5ee7776b3d5874e9c30c48f0f9bb4a88988017d
Peter Vu <vutuananh.vn.sg@gmail.com>
2020-12-23 00:00:44 +0800

    Final

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,42 @@
 <template>
   <h1>Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
   <!-- <p v-if="showResult">Reaction time: {{score}} ms</p> -->
-  <Results v-if="showResult" :score="score"/>
+  <Results v-if="showResult" :score="score" />
 </template>
 
 <script>
-import Block from '@/components/Block'
-import Results from '@/components/Results'
+import Block from "@/components/Block";
+import Results from "@/components/Results";
 
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
       isPlaying: false,
       delay: null,
       score: null,
-      showResult: false
-    }
+      showResult: false,
+    };
   },
   components: {
     Block,
-    Results
+    Results,
   },
   methods: {
     start() {
-      this.delay = 2000 + Math.random() * 5000
-      this.isPlaying = true
-      this.showResult = false
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResult = false;
     },
     endGame(reactionTime) {
-      this.score = reactionTime
-      this.isPlaying = false
-      this.showResult = true
-    }
-  }
-}
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResult = true;
+    },
+  },
+};
 </script>
 
 <style>
@@ -47,5 +47,20 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+button {
+  /* width: 400px; */
+  background: #0faf87;
+  color: white;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 40px auto;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="results">
       <p>Reaction time: {{score}} ms</p>
+      <p class="rank">{{rank}}</p>
   </div>
 </template>
 
@@ -10,12 +11,27 @@ export default {
     props: ['score'],
     data() {
         return {
-            // reactionTime: null
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score < 250){
+            this.rank = "Ninja Fingers"
+        }
+        else if(this.score < 400){
+            this.rank = "Rapid Reflexes"
+        }
+        else {
+            this.rank = "Snail Pace..."
         }
     }
 }
 </script>
 
 <style>
-
+.rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+}
 </style>
